fb9c5bbb744e0d27780d82263a3ce37a7a8b4533
Ermaotie <63703577+Ermaotie@users.noreply.github.com>
2021-03-25 00:31:16 +0800

    Update code2session.js

--- app/routes/code2session.js
+++ app/routes/code2session.js
@@ -3,11 +3,25 @@ module.exports = function (app,mongodb,conf) {
         console.log(req.query)
         if(req.query.openid){
             var collection = mongodb.collection('openid');
-            collection.insertOne({openid:req.query.openid},(err,result)=>{
-                if(err){
+            var filter = {openid:req.query.openid};
+            collection.find(filter).toArray(function (err,res) {
+                if (err) {
                     throw err;
+                } else {
+                    if(res.length==0){
+                        collection.insertOne(filter,(err,result)=>{
+                            if(err){
+                                throw err;
+                            } else {
+                                console.log("Inserted Openid!");
+                            }
+                        });
+                    }else{
+                        console.log("已存在该记录");
+                    }
                 }
-            });
+            })
+            
             res.send("Success");
         } else{
             res.send("Default");
